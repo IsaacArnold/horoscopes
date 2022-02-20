e28756d39a93fe3e5cf0a5c97ef87b7fca2ab032
Isaac Arnold <isaacarnold@icloud.com>
2022-02-20 20:11:38 +1000

    Applying font and brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-color": "#404493",
+        "secondary-color": "#DEDDFD",
+      },
+    },
   },
   plugins: [],
 };
